e87a145ca00d5ebc52ae4b5213428538ec6d5b8d
felipeargento <felipe@cartesi.io>
2020-05-27 17:59:44 +0800

    Add documentation

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -21,17 +21,17 @@
 
 
 /// @title Cartesi Staking
+/// @author Felipe Argento
 pragma solidity ^0.5.0;
 
 contract Staking {
     uint256 constant TIME_TO_STAKE = 5 days;
     uint256 constant TIME_TO_WITHDRAW = 5 days;
 
-    mapping(address => uint256) internal stakedBalance;
-    mapping(address => StakeStruct) internal toBeStakedList;
-    mapping(address => StakeStruct) internal toWithdrawList;
-    mapping(address => uint256) internal withdrawBalance;
-    mapping(address => ieoStruct) internal ieoFrozenFunds;
+    mapping(address => uint256) internal stakedBalance; // the amount of money currently being staked.
+    mapping(address => StakeStruct) internal toBeStakedList; // stakes that are waiting to be old enough to become staked.
+    mapping(address => StakeStruct) internal toWithdrawList; // money that is waiting to be withdrew.
+    mapping(address => ieoStruct) internal ieoFrozenFunds; // funds that were frozen during token launch, they will count as stake.
 
     struct ieoStruct {
         uint256 balance; // the amount of money that was locked during token creation
@@ -45,12 +45,16 @@ contract Staking {
         uint256 lastSearchIndex;
     }
 
-    function startStake(uint256 _amount) public {
+    /// @notice Deposit CTSI to be staked. The money will turn into staked balance after TIME_TO_STAKE days, if the function finalizeStakes is called.
+    /// @param _amount The amount of tokens that are gonna be deposited.
+    function depositStake(uint256 _amount) public {
         toBeStakedList[msg.sender].amount.push(_amount);
         toBeStakedList[msg.sender].date.push(now);
         toBeStakedList[msg.sender].count++;
     }
 
+    /// @notice Finalizes Stakes. Goes through the list toBeStaked and transform that into staked balance, if the requirements are met.
+    /// @dev The number of stakes finalized is limited to 50 in order to avoid a deadlock in the contract - when the list is big enough so that the iteration doesnt fit the gas limit.
     function finalizeStakes() public {
         StakeStruct memory TBSL = toBeStakedList[msg.sender];
 
@@ -66,12 +70,16 @@ contract Staking {
         }
     }
 
+    /// @notice Start CTSI withdraw from staked balance process. The money will turn into withdrawal balance after TIME_TO_WITHDRAW days, if the function finalizeWithdraw is called.
+    /// @param _amount The amount of tokens that are gonna be withdrew.
     function startWithdraw(uint256 _amount) public {
         toWithdrawList[msg.sender].amount.push(_amount);
         toWithdrawList[msg.sender].date.push(now);
         toWithdrawList[msg.sender].count++;
     }
 
+    /// @notice Finalizes withdraws. Goes through the list toWithdraw and removes that from staked balance, if the requirements are met.
+    /// @dev The number of withdraws finalized is limited to 50 in order to avoid a deadlock in the contract - when the list is big enough so that the iteration doesnt fit the gas limit.
     function finalizeWithdraws() public {
         StakeStruct memory TBWL = toWithdrawList[msg.sender];
 
