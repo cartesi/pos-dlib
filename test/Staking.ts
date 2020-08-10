73d9d2acf1b4677f49a6c64fd6408d3efe6b14f7
felipeargento <felipe@cartesi.io>
2020-08-10 18:08:28 +0800

    Fix Token import

--- test/Staking.ts
+++ test/Staking.ts
@@ -1,59 +1,75 @@
-import { describe } from "mocha";
 import { assert, expect, use } from "chai";
-import { deployments, ethers, getNamedAccounts } from "@nomiclabs/buidler";
+import {
+    deployments,
+    ethers,
+    getNamedAccounts,
+    getChainId,
+} from "@nomiclabs/buidler";
 import { solidity, MockProvider } from "ethereum-waffle";
 
 import { Staking } from "../src/types/Staking";
 import { Ierc20 } from "../src/types/Ierc20";
 import { Signer, ContractFactory } from "ethers";
 
+const { advanceTime } = require("./utils");
+
+const CTSI = require("@cartesi/token/build/contracts/CartesiToken.json");
+
 use(solidity);
 
 describe("Staking", async () => {
     const INDEX = 0;
+    const DAY = 86401; // day + 1 second
+
     let signer: Signer;
 
-    let staking_address: string;
-    let ctsi_address: string;
+    let stakingAddress: string;
     let factory: ContractFactory;
+    let ctsiFactory: ContractFactory;
 
     let instance: Staking;
     let ctsi: Ierc20;
 
     beforeEach(async () => {
         await deployments.fixture();
+        const network_id = await getChainId();
+
+        const CTSIAddress = CTSI.networks[network_id].address;
 
         [signer] = await ethers.getSigners();
 
-        staking_address = (await deployments.get("Staking")).address;
-        ctsi_address = (await deployments.get("CTSI")).address;
-        factory = await ethers.getContractFactory("Staking");
-        instance = factory.connect(signer).attach(staking_address) as Staking;
-        ctsi = factory.connect(signer).attach(ctsi_address) as Ierc20;
+        stakingAddress = (await deployments.get("Staking")).address;
 
-        // TODO: send CTSI tokens to alice
+        factory = await ethers.getContractFactory("Staking");
+        ctsiFactory = await ethers.getContractFactory("Ierc20");
+        instance = factory.connect(signer).attach(stakingAddress) as Staking;
+        ctsi = ctsiFactory.connect(signer).attach(CTSIAddress) as Ierc20;
     });
 
     it("function depositStake()", async () => {
         const { alice } = await getNamedAccounts();
+        let balance = await ctsi.balanceOf(alice);
+        console.log(balance);
+
+        ctsi.transfer(alice, 500000000);
 
         let currentBalance = await ctsi.balanceOf(alice);
         let toBeDeposited = 5;
 
         expect(
-            await ctsi.balanceOf(staking_address),
+            await ctsi.balanceOf(stakingAddress),
             "staking contract should be instantiated with zero CTSI"
         ).to.equal(0);
 
         // Alice has to allow contract to act send tokens on her behalf
-        await ctsi.increaseAllowance(staking_address, toBeDeposited);
+        await ctsi.increaseAllowance(stakingAddress, toBeDeposited);
 
         // TODO: this should check for events,
         // but contract is not emitting them yet
         await instance.depositStake(INDEX, toBeDeposited);
 
         expect(
-            await ctsi.balanceOf(staking_address),
+            await ctsi.balanceOf(stakingAddress),
             "toBeDeposited should be transferred to staking contract"
         ).to.equal(toBeDeposited);
 
@@ -68,7 +84,7 @@ describe("Staking", async () => {
         let toBeDeposited = 5;
 
         // Alice has to allow contract to act send tokens on her behalf
-        await ctsi.increaseAllowance(staking_address, 4 * toBeDeposited);
+        await ctsi.increaseAllowance(stakingAddress, 4 * toBeDeposited);
 
         await instance.finalizeStakes(INDEX);
         expect(
@@ -122,7 +138,7 @@ describe("Staking", async () => {
         const { alice } = await getNamedAccounts();
         let toBeDeposited = 5;
         // Alice has to allow contract to act send tokens on her behalf
-        await ctsi.increaseAllowance(staking_address, 5 * toBeDeposited);
+        await ctsi.increaseAllowance(stakingAddress, 5 * toBeDeposited);
 
         await expect(
             instance.startWithdraw(INDEX, toBeDeposited),
@@ -147,12 +163,12 @@ describe("Staking", async () => {
         ).to.equal(toBeDeposited);
 
         await instance.startWithdraw(INDEX, toBeDeposited);
- 
+
         expect(
             await instance.getStakedBalance(INDEX, alice),
             "staked balance should diminish right after withdraw starts"
         ).to.equal(0);
-       
+
         expect(
             await ctsi.balanceOf(alice),
             "tokens should only return to alice after withdraw was finalized"
@@ -172,7 +188,7 @@ describe("Staking", async () => {
             await ctsi.balanceOf(alice),
             "tokens should return after a successful finalizeWithdraws()"
         ).to.greaterThan(balanceAfterDeposit);
-        
+
         // test multiple withdraws
 
         await instance.depositStake(INDEX, toBeDeposited);
@@ -190,7 +206,7 @@ describe("Staking", async () => {
         await instance.startWithdraw(INDEX, toBeDeposited);
         await instance.startWithdraw(INDEX, 2 * toBeDeposited);
         await instance.startWithdraw(INDEX, toBeDeposited);
-        
+
         // TODO: advance time here
         await instance.finalizeWithdraws(INDEX);
 
@@ -200,9 +216,8 @@ describe("Staking", async () => {
         ).to.equal(0);
 
         expect(
-            await ctsi.balanceOf(staking_address),
+            await ctsi.balanceOf(stakingAddress),
             "there should be no money left on staking contract"
         ).to.equal(0);
-
     });
 });
