20beba4ea668740122d2074efea43bdcd42c0540
felipeargento <felipe@cartesi.io>
2020-08-10 16:50:39 +0800

    Add utils with advanceTime function

---
+++ test/utils.js
@@ -0,0 +1,17 @@
+const { ethers } = require("@nomiclabs/buidler");
+const { expect, use } = require("chai");
+const { solidity } = require("ethereum-waffle");
+const axios = require("axios");
+
+use(solidity);
+
+
+const advanceTime = async (provider, seconds) => {
+  const payload = {
+    method: "evm_increaseTime",
+    params: [seconds],
+    jsonrpc: "2.0",
+    id: 0,
+  };
+  await axios.post(provider._buidlerProvider._url, payload);
+};
