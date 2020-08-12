5254b10e04db3ddb977dc77f9d7f9cedf67c4f91
Gabriel Barros <gabriel.barros@cartesi.io>
2020-08-11 22:31:13 -0300

    Fix advanceTime util to work with internal process node

--- test/utils.js
+++ test/utils.js
@@ -1,19 +1,11 @@
-const { ethers } = require("@nomiclabs/buidler");
-const { expect, use } = require("chai");
+const { use } = require("chai");
 const { solidity } = require("ethereum-waffle");
-const axios = require("axios");
 
 use(solidity);
 
 
 const advanceTime = async (provider, seconds) => {
-  const payload = {
-    method: "evm_increaseTime",
-    params: [seconds],
-    jsonrpc: "2.0",
-    id: 0,
-  };
-  await axios.post(provider._buidlerProvider._url, payload);
+  await provider.send("evm_increaseTime", [seconds]);
 };
 module.exports = {
   advanceTime,
