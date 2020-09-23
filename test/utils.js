3c08b2d60b25f51e9298a14db1af2a8f07910006
felipeargento <felipe@cartesi.io>
2020-09-24 00:20:56 +0800

    Add advanceBlock function

--- test/utils.js
+++ test/utils.js
@@ -7,6 +7,12 @@ use(solidity);
 const advanceTime = async (provider, seconds) => {
   await provider.send("evm_increaseTime", [seconds]);
 };
+
+const advanceBlock = async (provider) => {
+  await provider.send("evm_mine");
+};
+
 module.exports = {
   advanceTime,
+  advanceBlock
 };
