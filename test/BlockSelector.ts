d34519439764821a155b8c0d93dc2dd82e417c79
felipeargento <felipe@cartesi.io>
2020-12-10 17:02:36 +0800

    Increase BlockSelector test coverage

--- test/BlockSelector.ts
+++ test/BlockSelector.ts
@@ -350,4 +350,116 @@ describe("BlockSelector", async () => {
             "selection was reset, block shouldnt be produced"
         ).to.equal(false);
     });
+
+    it("isConcerned should always return false", async () => {
+        let address = await signer.getAddress();
+
+        expect(
+            await blockSelector.isConcerned(0, address),
+            "user should never be concerned by blockselector"
+        ).to.equal(false);
+
+        await blockSelector.instantiate(
+            minDiff,
+            initialDiff,
+            diffAdjust,
+            targetInterval,
+            address
+        );
+        expect(
+            await blockSelector.isConcerned(0, address),
+            "user should never be concerned by blockselector"
+        ).to.equal(false);
+    });
+
+    it("getSubInstances should return empty", async () => {
+        let address = await signer.getAddress();
+
+        await blockSelector.instantiate(
+            minDiff,
+            initialDiff,
+            diffAdjust,
+            targetInterval,
+            address
+        );
+
+        var subInstances = await blockSelector.getSubInstances(0, address);
+
+        expect(subInstances[0].length).to.equal(0);
+        expect(subInstances[1].length).to.equal(0);
+    });
+
+    it("getters should return expected values", async () => {
+        let address = await signer.getAddress();
+        let mediumWeight = 5000000;
+
+        await blockSelector.instantiate(
+            minDiff,
+            initialDiff,
+            diffAdjust,
+            targetInterval,
+            address
+        );
+
+        var blockCount = await blockSelector.getBlockCount(0);
+        var fTimestamp = await blockSelector.getLastBlockTimestamp(0);
+        var difficulty = await blockSelector.getDifficulty(0);
+        var minDifficulty = await blockSelector.getMinDifficulty(0);
+        var adjustmentParam = await blockSelector.getDifficultyAdjustmentParameter(
+            0
+        );
+        var gTargetInterval = await blockSelector.getTargetInterval(0);
+        var microSeconds = await blockSelector.getMicrosecondsSinceLastBlock(0);
+
+        expect(blockCount, "blockCount should start at zero").to.equal(0);
+        expect(difficulty, "difficulty should start as initialDiffi").to.equal(
+            initialDiff
+        );
+        expect(
+            minDifficulty,
+            "getMinDiff should return minimumDifficulty"
+        ).to.equal(minDiff);
+        expect(
+            adjustmentParam,
+            "getAdjustmentParam should return adjustment parameter"
+        ).to.equal(diffAdjust);
+        expect(
+            gTargetInterval,
+            "getTargetInterval should return target interval"
+        ).to.equal(targetInterval);
+        expect(
+            microSeconds,
+            "micro seconds since last block should start at zero"
+        ).to.equal(0);
+
+        // advance blocks so the target is set
+        await advanceBlock(signer.provider);
+        await advanceBlock(signer.provider);
+
+        // advance time so a block can definitely be produced
+        await advanceTime(signer.provider, 24 * 60 * 60); // advance 1 day
+        await advanceBlock(signer.provider);
+
+        var sMicroSeconds = await blockSelector.getMicrosecondsSinceLastBlock(
+            0
+        );
+
+        expect(sMicroSeconds, "more than 1 day should have passed").to.be.above(
+            24 * 60 * 60 * 1000000
+        ); //microseconds
+        // produce a block
+        await blockSelector.produceBlock(0, address, mediumWeight);
+
+        var sTimestamp = await blockSelector.getLastBlockTimestamp(0);
+        blockCount = await blockSelector.getBlockCount(0);
+
+        expect(
+            sTimestamp,
+            "second timestamp should be bigger than first"
+        ).to.be.above(fTimestamp);
+        expect(
+            blockCount,
+            "blockCount should be one after first block is created"
+        ).to.be.equal(1);
+    });
 });
