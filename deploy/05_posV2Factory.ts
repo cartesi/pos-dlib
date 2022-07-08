5ea28c000d45bfeb8500b12454fb05c547d8102a
Stephen Chen <stephen.chen@cartesi.io>
2022-07-08 13:05:13 +0800

    Update deployment scripts

---
+++ deploy/05_posV2Factory.ts
@@ -0,0 +1,34 @@
+// Copyright 2022 Cartesi Pte. Ltd.
+
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+// this file except in compliance with the License. You may obtain a copy of the
+// License at http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+
+const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
+    const { deployments, getNamedAccounts } = hre;
+    const { deploy } = deployments;
+    const { deployer } = await getNamedAccounts();
+    const { Bitmask, Difficulty, Eligibility, Tree } = await deployments.all();
+
+    await deploy("PoSV2FactoryImpl", {
+        from: deployer,
+        log: true,
+        libraries: {
+            ["Bitmask"]: Bitmask.address,
+            ["Difficulty"]: Difficulty.address,
+            ["Eligibility"]: Eligibility.address,
+            ["Tree"]: Tree.address,
+        },
+    });
+};
+
+export default func;
+export const tags = ["PoSV2Factory"];
